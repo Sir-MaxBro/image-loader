0f0a2100155a3455f5300688b4f65a72b7eabce6
Maksim Chendrov <maxbrofenx@gmail.com>
2019-02-21 14:48:58 +0300

    1.5-ui-logic -- added print helper

---
+++ ImageLoader/ImageLoader.ConsoleUI/Helpers/IPrintHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ImageLoader.ConsoleUI.Helpers
+{
+    public interface IPrintHelper
+    {
+        void PrintItems<T>(string header, IReadOnlyCollection<T> items, Func<T, string> getPrintedItem);
+
+        void PrintText(string text);
+    }
+}
---
+++ ImageLoader/ImageLoader.ConsoleUI/Helpers/Implementations/PrintHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ImageLoader.ConsoleUI.Helpers.Implementations
+{
+    internal class PrintHelper : IPrintHelper
+    {
+        public void PrintItems<T>(string header, IReadOnlyCollection<T> items, Func<T, string> getPrintedItem)
+        {
+            Console.WriteLine();
+            Console.WriteLine(header);
+            foreach (var item in items)
+            {
+                Console.WriteLine(getPrintedItem(item));
+            }
+        }
+
+        public void PrintText(string text)
+        {
+            Console.WriteLine(text);
+        }
+    }
+}
