e079c28f6dece73dbf09edac96900aa67adfaa16
Maksim Chendrov <maxbrofenx@gmail.com>
2019-02-21 13:29:14 +0300

    1.3-ioc-container -- setup ioc container

---
+++ ImageLoader/ImageLoader.IoC/BootstrapContainer.cs
@@ -0,0 +1,21 @@
+﻿using ImageLoader.Shared;
+using StructureMap;
+using System;
+
+namespace ImageLoader.IoC
+{
+    public static class BootstrapContainer
+    {
+        private static Lazy<IContainer> _containerLazy = new Lazy<IContainer>(InitializeContainer);
+
+        public static IContainer Container => _containerLazy.Value;
+
+        private static IContainer InitializeContainer()
+        {
+            return new Container(c =>
+            {
+                c.AddRegistry<SharedRegistry>();
+            });
+        }
+    }
+}
