69dadf2ced1708ad0dcb78ad462cd01d56533d0f
Maksim Chendrov <maxbrofenx@gmail.com>
2019-02-21 13:33:29 +0300

    1.4-image-loader -- added needed models

---
+++ ImageLoader/ImageLoader.Business/Models/ImageInfoModel.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+
+namespace ImageLoader.Business.Models
+{
+    public class ImageInfoModel
+    {
+        public ImageInfoModel(string imageUrl, byte[] bytes)
+        {
+            this.Bytes = bytes;
+            this.ImageUrl = imageUrl;
+        }
+
+        public int ByteSize => this.Bytes != null ? this.Bytes.Length : default(int);
+
+        public int EndThreadId { get; set; }
+
+        public int StartThreadId { get; set; }
+
+        public long LoadedTime { get; set; }
+
+        public byte[] Bytes { get; }
+
+        public string ImageUrl { get; }
+
+        public override string ToString()
+        {
+            var message = new StringBuilder()
+                .AppendLine($"Image url: {this.ImageUrl}")
+                .AppendLine($"Byte size: {this.ByteSize}")
+                .AppendLine($"StartThreadId: {this.StartThreadId}")
+                .AppendLine($"EndThreadId: {this.EndThreadId}")
+                .AppendLine($"LoadedTime: {this.LoadedTime} ms");
+
+            return message.ToString();
+        }
+    }
+}
